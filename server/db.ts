8943416c6905d10385d60af58cfe20f925546085
chaubeychaubey234-max <chaubeychaubey234@gmail.com>
2026-01-20 22:12:42 +0530

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,14 +1,18 @@
-import { drizzle } from "drizzle-orm/node-postgres";
+import 'dotenv/config';
 import pg from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "@shared/schema";
 
 const { Pool } = pg;
 
-if (!process.env.DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
-  );
-}
+export const pool = new Pool({
+  host: "127.0.0.1",
+  user: "postgres",
+  password: "postgres",
+  database: "kid_learn",
+  port: 5432,
+  ssl: false,
+});
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle(pool, { schema });
+export type Database = typeof db;
